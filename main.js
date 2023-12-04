03c016f2e17831a1e51bd677adbe15324be116a1
Manan837 <89606852+Manan837@users.noreply.github.com>
2023-12-04 18:34:20 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 video ="";
 status1 = "";
+objects = [];
 
 function preload()
 {
@@ -16,6 +17,32 @@ function setup()
 function draw()
 {
     image(video, 0, 0, 480, 380);
+    if(status1 != "")
+    {
+        objectDetector.detect(video, gotResults);
+        for(i = 0; i < objects.length; i++)
+        {
+            document.getElementById("status").innerHTML = "Objects Detected";
+            document.getElementById("number_of_objects").innerHTML = "Number of Objects: " + objects.length;
+
+            fill("#FF0000");
+            percent = floor(objects[i].confidence * 100);
+            text(objects[i].label + "" + percent + "%", objects[i].x - 15, objects[i].y);
+            noFill();
+            stroke("#FF0000");
+            rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+        }
+    }
+}
+
+function gotResults(error, results)
+{
+    if(error)
+    {
+        console.log(error);
+    }
+    console.log(results);
+    objects = results;
 }
 
 function start()
